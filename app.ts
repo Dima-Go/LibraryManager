8dd9d76773cd2c5be79bd27cb2ea8db369e8a08a
Arthur <my.towell42@gmail.com>
2018-05-14 10:37:19 +0300

    First script - no typing.
    - functions
    - let and const usage

--- app.ts
+++ app.ts
@@ -1,6 +1,28 @@
-class HelloWorld {
-    constructor(public message: string) { }
+function GetAllBooks() {
+    let books = [
+        { title: 'Ulysses', author: 'James Joyce', available: true },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
+        { title: 'A Prisoner of Bitrh', author: 'Jeffrey Archer', available: true },
+        { title: 'Moby Dick', author: 'Herman Melville', available: true }
+    ]
+
+    return books;
 }
 
-var hello = new HelloWorld('Hello TypeScript');
-console.log(hello.message);
+function LogFirstAvailable(books) {
+    let numberOfBooks = books.length;
+    let firstAvailable = '';
+
+    for (let currentBook of books) {
+
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    console.log('Total Books: ' + numberOfBooks);
+    console.log('First Available: ' + firstAvailable);
+}
+
+const library = GetAllBooks();
+LogFirstAvailable(library);

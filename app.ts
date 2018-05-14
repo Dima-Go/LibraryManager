c443355d4811ed58e931235cd4c08ccc6b3b8cdf
Arthur <my.towell42@gmail.com>
2018-05-14 11:12:38 +0300

    Function return types and enums
    - Added category enum
    - Added return types to functions
    - Added tsconfig.json to eliminate duplicate function compiler error

--- app.ts
+++ app.ts
@@ -1,17 +1,20 @@
+enum Category { Biography, Poetry, Fiction, History }
+
 function GetAllBooks() {
     let books = [
-        { title: 'Ulysses', author: 'James Joyce', available: true },
-        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
-        { title: 'A Prisoner of Bitrh', author: 'Jeffrey Archer', available: true },
-        { title: 'Moby Dick', author: 'Herman Melville', available: true }
+        { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { title: 'A Prisoner of Bitrh', author: 'Jeffrey Archer', available: true, category: Category.Fiction },
+        { title: 'Moby Dick', author: 'Herman Melville', available: false, category: Category.Fiction },
+        { title: 'Steve Jobs', author: 'Walter Isaacson', available: true, category: Category.Biography }        
     ]
 
     return books;
 }
 
-function LogFirstAvailable(books) {
-    let numberOfBooks = books.length;
-    let firstAvailable = '';
+function LogFirstAvailable(books: any[]): void {
+    let numberOfBooks: number = books.length;
+    let firstAvailable: string = '';
 
     for (let currentBook of books) {
 
@@ -24,5 +27,24 @@ function LogFirstAvailable(books) {
     console.log('First Available: ' + firstAvailable);
 }
 
-const library = GetAllBooks();
-LogFirstAvailable(library);
+function GetBookTitlesByCategory(categoryFilter: Category): Array<string> {
+    console.log('Getting books of category: ' + Category[categoryFilter]);
+    const library = GetAllBooks();
+    const filteredTitles: string[] = [];
+
+    for (let currentBook of library) {
+        if (currentBook.category == categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+
+    return filteredTitles;
+}
+
+function LogBookTitle(titles: string[]): void {
+    for (let title of titles) {
+        console.log(title);
+    }
+}
+ const biographyBooks = GetBookTitlesByCategory(Category.Biography);
+ LogBookTitle(biographyBooks);
